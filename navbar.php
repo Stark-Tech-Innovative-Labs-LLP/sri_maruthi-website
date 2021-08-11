7e4ef37371611723126ce694e9aa57dc031f9fe5
tharun bp <bpatharun@gmail.com>
2021-08-12 00:21:33 +0530

    cleared a bug in navbar

--- navbar.php
+++ navbar.php
@@ -36,11 +36,5 @@
 
     </div>
 
-        <section style="  background-image: url('assets/img/courses/head.jpg') top center;" id="hero" class="d-flex justify-content-center align-items-center">
-    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Learning Today,<br>Leading Tomorrow</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
-      <a href="courses.html" class="btn-get-started">Get Started</a>
-    </div>
-  </section>
+        
   </header>
