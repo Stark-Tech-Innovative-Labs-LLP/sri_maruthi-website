517dff38bde90bcd26a496abcff053f02b595105
tharun bp <bpatharun@gmail.com>
2021-08-12 00:07:25 +0530

    init 2

---
+++ navbar.php
@@ -0,0 +1,46 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <a href="index.php" class="logo me-auto"><img src="./assets/img/logo.png" alt="" class="Logo"></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a id="home" href="./index.php">Home</a></li>
+          <li><a id="about" href="./about.php">About</a></li>
+          <li><a id="gallery" href="./gallery.php">Gallery</a></li>
+          <li><a id="event" href="./events.php">Events</a></li>
+          <li><a id="courses" href="./courses.php">Courses</a></li>
+          <li><a id="contact" href="./contact.php">Contact</a></li>
+          <li class="dropdown">
+            <a href="#"><span>Resource</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li>
+                <li><a id="admission" href="./admission.php">Admission InQuery</a></li>
+                <li><a id="study-material" href="./study-material.php">Study Material</a></li>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#"><span>Fields</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li>
+                <li><a id="pu" href="./pu.php">PU</a></li>
+                <li><a id="degree" href="./degree.php">Degree</a></li>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+      <a href="courses.php" class="get-started-btn">Get Started</a>
+
+    </div>
+
+        <section style="  background-image: url('assets/img/courses/head.jpg') top center;" id="hero" class="d-flex justify-content-center align-items-center">
+    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Learning Today,<br>Leading Tomorrow</h1>
+      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <a href="courses.html" class="btn-get-started">Get Started</a>
+    </div>
+  </section>
+  </header>
