c337de16c5921d2d3502a0b6456c5277353d3fbf
tharun bp <bpatharun@gmail.com>
2021-08-12 10:24:02 +0530

    Updated Contact and courses page

--- study-material.php
+++ study-material.php
@@ -25,21 +25,21 @@
   <img src="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_view_of_the_sun2/22133123-1-eng-GB/Solar_Orbiter_s_first_view_of_the_Sun_pillars.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Languages Study Materials [PUC]</h5>
-    <a href="waste?class=LANG"  class="btn btn-danger">View</a>
+    <a href="Materials?class=LANG"  class="btn btn-danger">View</a>
   </div>
 </div></div> 
 <div class="col-lg-4"><div class="card" style="width: 18rem;">
   <img src="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_view_of_the_sun2/22133123-1-eng-GB/Solar_Orbiter_s_first_view_of_the_Sun_pillars.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Science Study Materials [PUC]</h5>
-    <a href="waste?class=PUSC"  class="btn btn-danger">View</a>
+    <a href="Materials?class=PUSC"  class="btn btn-danger">View</a>
   </div>
 </div></div>
         <div class="col-lg-4"><div class="card" style="width: 18rem;">
   <img src="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_view_of_the_sun2/22133123-1-eng-GB/Solar_Orbiter_s_first_view_of_the_Sun_pillars.png" class="card-img-top" alt="...">
   <div class="card-body">
   <h5 class="card-title">Commerece Study Materials [PUC]</h5>
-    <a href="waste?class=PUCM"  class="btn btn-primary">View</a>
+    <a href="Materials?class=PUCM"  class="btn btn-primary">View</a>
   </div>
 </div></div>
     </div>
@@ -53,7 +53,7 @@
   <img src="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_view_of_the_sun2/22133123-1-eng-GB/Solar_Orbiter_s_first_view_of_the_Sun_pillars.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">B.Com Study Materials </h5>
-    <a href="waste?class=DEG"  class="btn btn-success">View</a>
+    <a href="Materials?class=DEG"  class="btn btn-success">View</a>
   </div>
 </div></div>
 <div class="col-lg-3"></div>
