beec66fe33238475635cb02c5d3af112b443c397
codedbyyashwanth <61375676+codedbyyashwanth@users.noreply.github.com>
2021-09-18 11:25:03 +0530

    Changed Title in Every Page

--- Docs.php
+++ Docs.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
  
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Document</title>
+        <title>Sri Maruthi PU College - Moulding Better Tomorrow through Education & Excellence</title>
+
     <style>
         #contact {
             color: #3E64FF;
--- about.php
+++ about.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sri Maruthi PU College</title>
+        <title>Sri Maruthi PU College - Moulding Better Tomorrow through Education & Excellence</title>
     <?php require("./header.php") ?>
     <style>
         #about
